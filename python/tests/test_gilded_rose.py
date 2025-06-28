b7350dc73b5068ddeadd77f161b2e187dfed6f81
Abhi199622 <abhishekgoyal1296@gmail.com>
2025-06-28 15:18:10 +0530

    set up config.gr for approval tests and approved file updated

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -5,11 +5,17 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
+    def test_base_item_quality(self):
+        item = Item(name="foo", sell_in=4, quality=4)
+        gilded_rose = GildedRose([item])
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual(3, item.quality)
+
+    def test_base_item_quality_sell_by_date_passed(self):
+        item = Item(name="foo", sell_in=0, quality=4)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertEqual(2, item.quality)
 
         
 if __name__ == '__main__':
